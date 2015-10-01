394df0523da82f51f282b91e7b13b80229fbdb6a
Aleksandr <aleksandr_mdc@mail.ru>
2015-10-02 00:07:48 +0400

    Хочу прикрутить прогресс бар на таймер

--- app/src/main/java/com/aleksandrp/mystopwatch/fragments/HistoryFragment.java
+++ app/src/main/java/com/aleksandrp/mystopwatch/fragments/HistoryFragment.java
@@ -1,19 +1,23 @@
 package com.aleksandrp.mystopwatch.fragments;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.aleksandrp.mystopwatch.R;
+import com.github.lzyzsd.circleprogress.ArcProgress;
+import com.github.lzyzsd.circleprogress.DonutProgress;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HistoryFragment extends Fragment {
 
+    ArcProgress arcProgress1;
+    DonutProgress arcProgress2;
 
     public HistoryFragment() {
         // Required empty public constructor
@@ -23,8 +27,19 @@ public class HistoryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_history, container, false);
+
+        arcProgress1 = (ArcProgress) view.findViewById(R.id.arc_progress);
+        arcProgress2 = (DonutProgress) view.findViewById(R.id.arc_progress1);
+
+        arcProgress1.setMax(5555);
+        arcProgress1.setProgress(111);
+
+        arcProgress2.setMax(250);
+        arcProgress2.setProgress(25);
+
+        return view;
     }
 
 
--- app/src/main/java/com/aleksandrp/mystopwatch/fragments/TimerFragment.java
+++ app/src/main/java/com/aleksandrp/mystopwatch/fragments/TimerFragment.java
@@ -1,20 +1,24 @@
 package com.aleksandrp.mystopwatch.fragments;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.aleksandrp.mystopwatch.R;
+import com.github.lzyzsd.circleprogress.DonutProgress;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TimerFragment extends Fragment {
 
-
+    private DonutProgress donutProgress;
 
     public TimerFragment() {
         // Required empty public constructor
@@ -24,8 +28,43 @@ public class TimerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_timer, container, false);
+        View view = inflater.inflate(R.layout.fragment_timer, container, false);
+        donutProgress = (DonutProgress) view.findViewById(R.id.donut_progress);
+
+        donutProgress.setMax(1000);
+
+        count();
+
+        return view;
+    }
+
+    private void count() {
+        new TimerTask().execute();
+    }
+    class TimerTask extends AsyncTask {
+        long timeFromStart = 0;
+        int res = 0;
+
+        @Override
+        protected Object doInBackground(Object[] params) {
+//             startTime = System.currentTimeMillis();
+            while (res<1000) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                publishProgress();
+                res++;
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Object[] values) {
+            super.onProgressUpdate(values);
+            donutProgress.setProgress(res);
+        }
     }
 
 
